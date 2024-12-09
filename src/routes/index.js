80e3a71aec01d274621b89c5a740a735b19e353f
marielly-garcia-jimenez <131173711+marielly-garcia-jimenez@users.noreply.github.com>
2024-12-09 00:04:09 -0600

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,14 @@
-
 const express = require('express');
 const router = express.Router();
 
 const estudiantesRutas = require('./estudiantesRutas');
 const calificacionesRutas = require('./calificacionesRutas');
-const MateriaRutas = require('./materiasRutas');
+const materiaRutas = require('./materiasRutas');
 const maestrosRutas = require('./maestrosRutas');
 
 router.use('/estudiantes', estudiantesRutas);
 router.use('/calificaciones', calificacionesRutas);
-router.use('/materias', MateriaRutas);
+router.use('/materias', materiaRutas);
 router.use('/maestros', maestrosRutas)
 
 
